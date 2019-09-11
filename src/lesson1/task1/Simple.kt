89c4b41a3747b1dcd6df7585c9749d8b2d106550
сергей <sergey_fedorov200001@mail.ru>
2019-09-11 23:56:25 +0300

    lesson1 and lesson2 - one task

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -59,7 +59,7 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = 3600 * hours + 60 * minutes+ seconds
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = 3600 * hours + 60 * minutes + seconds
 
 /**
  * Тривиальная
@@ -68,7 +68,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = 3600 * hours + 60 * m
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = 4.445 / 100 * (sagenes * 48 + arshins * 16 + vershoks )
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double =
+    4.445 / 100 * (sagenes * 48 + arshins * 16 + vershoks)
 
 /**
  * Тривиальная
@@ -103,7 +104,7 @@ fun thirdDigit(number: Int): Int = (number / 100) % 10
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
 fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =
-    hoursArrive * 60 + minutesArrive -hoursDepart * 60 - minutesDepart
+    hoursArrive * 60 + minutesArrive - hoursDepart * 60 - minutesDepart
 
 /**
  * Простая
@@ -112,7 +113,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial * ((percent / 100.0 +1).pow(3))
+fun accountInThreeYears(initial: Int, percent: Int): Double = initial * ((percent / 100.0 + 1).pow(3))
 
 /**
  * Простая
